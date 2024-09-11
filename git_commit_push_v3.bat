c0f70727e8a867267a7665511b12c4a90d5e891a
unknown <dysontilano@gmail.com>
2024-09-11 13:37:29 -0500

    Added script 'git_commit_push_v3.bat'

---
+++ git_commit_push_v3.bat
@@ -0,0 +1,30 @@
+@echo off
+set currentPath="%cd%"
+set messsage="%1"
+set gitPath="%2"
+set currentPath=%currentPath:"=%
+set messsage=%messsage:"=%
+set gitPath=%gitPath:"=%
+if not "%messsage%" == "" (
+    echo Current path = "%currentPath%"
+    echo Message = "%messsage%"
+    if not "%gitPath%" == "" (
+        echo Git path = "%gitPath%"
+        cd "%gitPath%"
+        echo New path = "%gitPath%"
+    ) else (
+        echo Git path not specified using current path
+    )
+    ::dir > out.tmp
+    echo.
+    ::echo git add .
+    ::echo git commit -m "%messsage%"
+    ::echo git push
+    git add . && git commit -m "%messsage%" && git push -f
+    cd "%currentPath%"
+    echo.
+    echo.
+    echo.
+) else (
+    echo Message not specified
+)
